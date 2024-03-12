71c6210cb7297cd7f9f9dc1e9a75b7310088b737
PaveTheWay123 <145612808+PaveTheWay123@users.noreply.github.com>
2024-03-12 16:41:32 +0200

    Update event.data.js

--- event.data.js
+++ event.data.js
@@ -1,17 +1,52 @@
 /* Modify this object to add new events to your page
 *  Example object:
     {
-        "name": "Sample Event",
-        "dateString": "27th of July 2024",
-        "description": "Some HTML event description here Some HTML event description here Some HTML event description here Some HTML event description here",
-        "src": "images/placeholder.jpeg"
+        "name": "Event 1",
+        "dateString": "8th of June 2023",
+        "description": "Rawane's Gig at Twenty-Two Gemmeze",
+        "src": "images/Event 1.jpeg"
     }
 * */
 const EVENT_DATA = [
     {
-        "name": "Sample Event",
-        "dateString": "27th of July 2024",
-        "description": "Some HTML event description here Some HTML event description here Some HTML event description here Some HTML event description here",
-        "src": "images/placeholder.jpeg"
+        "name": "Event 2",
+        "dateString": "7th of July 2023",
+        "description": "JP Sikias's Performance at STOA Mar Mkhael",
+        "src": "images/Event 2.jpeg"
     }
 ];
+
+    {
+        "name": "Event 3",
+        "dateString": "27th - 30th of July 2023",
+        "description": "Bake Sale at Diaspora Village",
+        "src": "images/Event 3.jpeg"
+    }
+
+    {
+        "name": "Event 4",
+        "dateString": "12th - 14th of August 2023",
+        "description": "Bake Sale in Jezzine",
+        "src": "images/Event 4.jpeg"
+    }
+
+    {
+        "name": "Event 5",
+        "dateString": "22nd of Ocotber 2023",
+        "description": "Rawane's Gig at Twenty-Two Gemmeyze",
+        "src": "images/Event 5.jpeg"
+    }
+
+    {
+        "name": "Event 6",
+        "dateString": "11th of December 2023",
+        "description": "Theatre Play at ALBA",
+        "src": "images/Event 6.jpeg"
+    }
+
+    {
+        "name": "Up Coming Event",
+        "dateString": "",
+        "description": "Stay Up To Date for our Upcoming Events",
+        "src": "images/pave-the-way-logo-2.jpeg"
+    }
